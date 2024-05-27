566e26b37497879a626ef16728205d1a9b46840a
sahil <igsahilsayyad@gmail.com>
2024-05-28 01:27:57 +0530

    footer section component done.

--- src/components/HomepageFootersection/index.jsx
+++ src/components/HomepageFootersection/index.jsx
@@ -1,3 +1,4 @@
+import { Heading } from "components/Heading";
 import { Img } from "components/Img";
 import { Text } from "components/Text";
 import React from "react";
@@ -15,16 +16,102 @@ export default function HomepageFootersection(){
                                     TechVolt: Unleashing Innovation, Redefining Convenience - Your Ultimate Destination for Seamless
                                     Tech Exploration and Empowerment.
                                 </Text>
+                                <div className="flex items-center gap-2.5 self-stretch">
+                                    <Img src="images/img_lock_black_900_02.svg" alt="security icon" className="h-[24px] w-[24px] self-start"/>
+                                    <Text as="p" className="self-end">
+                                        TechVolt@gamil.com
+                                    </Text>
+
+                                </div>
 
                             </div>
+                        </div>
+                        <div className="flex w-[31%] items-start justify-between gap-5 md:w-full sm:flex-col">
+                            <div className="flex flex-col gap-3">
+                                    <Heading as="h6" className="!text-black-900_02">
+                                        Sitemap
+                                    </Heading>
+                                    <ul className="flex flex-col gap-[13px]">
+                                        <li>
+                                            <a href="#">
+                                                <Text as="p">Product</Text>
+                                            </a>
+                                        </li>
+                                        <li>
+                                            <a href="#">
+                                                <Text as="p">Services</Text>
+                                            </a>
+                                        </li>
+                                        <li>
+                                            <a href="#">
+                                                <Text as="p">Article</Text>
+                                            </a>
+                                        </li>
+                                        <li>
+                                            <a href="#">
+                                                <Text as="p">About Us</Text>
+                                            </a>
+                                        </li>
+
+
+                                    </ul>
+                           
+                            </div>
+                            <div className="flex flex-col items-start gap-3">
+                                <Heading as="h6" className="!text-black-900_02">
+                                    Information
+                                </Heading>
+                                <ul className="flex flex-col items-start gap-[13px]">
+                                <li>
+                                        <a href="#">
+                                            <Text as="p">Privacy Policy</Text>
+                                        </a>
+                                    </li>
+                                    <li>
+                                        <a href="#">
+                                            <Text as="p">Terms & Conditions</Text>
+                                        </a>
+                                    </li>
+                                    <li>
+                                        <a href="#">
+                                            <Text as="p">Contact</Text>
+                                        </a>
+                                    </li>
+                                    <li>
+                                        <a href="#">
+                                            <Text as="p">FAQs</Text>
+                                        </a>
+                                    </li>
+
+                                </ul>
+
+                            </div>
+                        
+                     </div>
+
+                
+                <div className="flex w-[19%] flex-col items-start gap-3.5 md:w-full">
+
+                        <Heading as="h6" className="!text-black-900_02">
+                            Connect with Us
+                        </Heading>
+
+                
+                        <div className="flex w-[52%] justify-between gap-5  md:w-full">
+                            <Img src="images/img_bx_bxl_instagram_alt.svg" alt="instagram icon" className="h-[24px] w-[24px]"/>
+                            <Img src="images/img_akar_icons_twitter_fill.svg" alt="twitter icon" className="h-[24px] w-[24px]"/>
+                            <Img src="images/img_akar_icons_linkedin_fill.svg" alt="linkedin icon" className="h-[24px] w-[24px]"/>
                         </div>
 
                     </div>
 
                 </div>
-
-                             
-            </div>
-        </footer>
+                <div className="mt-[60px] h-px w-full text-center self-stretch bg-gray-400_01"></div>
+                        <Text as="p" className="mt-[50px] !text-black-900_cc">
+                            @ Copyright 2024 TechVolt. All rights reserved
+                        </Text>
+            </div>                  
+        </div>
+    </footer>
     )
 }
---
+++ src/components/Input/index.jsx
@@ -0,0 +1,92 @@
+import React from "react";
+import PropTypes from "prop-types"
+
+const shapes = {
+    square: "rounded-[0px]",
+    round: "rounded-[25px]",
+};
+
+const variants = {
+
+    fill: {
+        white_A700: "bg-white-A700 text-gray-700",
+        gray_300_01: "bg-gray-300_01 text-gray-700",
+    },
+    outline: {
+        black_900_02: "border-black-900_02 border-b border-solid text-gray-700",
+        blue_gray_100_01: "border-blue_gray-100_01 border border-solid text-gray-700", 
+    },
+};
+
+
+const sizes = {
+    xs: "h-[39px] pr-[35px] text-lg",
+    md: "h-[51px] px-6 text-lg",
+    lg: "h-[60px] px-[15px] text-base",
+    sm: "h-[50px] px-3.5 text-sm",
+};
+
+const Input = React.forwardRef(
+    (
+     {
+        
+        children,
+        name="",
+        className = "",
+        placeholder="",
+        type="text",
+        label,
+        prefix,
+        suffix,
+        onChange ,
+        shape,
+        variant = "fill", 
+        size = "sm",
+        color = "white_A700",
+        ...restProps
+        },
+        ref,
+    ) => {
+        const handleChange = (e) => {
+            if(onChange) onChange(e?.target?.value);
+        } ;
+
+       
+        return (
+            <>
+            <label
+                className={`${className} flex  items-center justify-center cursor-text border-solid ${(shape && shapes[shape]) || ""} ${(variants[variant]?.[color]) || variants[variant] || ""}${(size && sizes[size]) || ""}`}
+            >
+            {!!label && label}
+            {!!prefix && prefix}
+            <input ref={ref} type={type} name={name} onChange={handleChange} placeholder={placeholder} {...restProps}/>
+            {!!suffix && suffix}  
+            </label>
+            </>
+        );
+    },
+);
+
+
+
+
+Input.propTypes = {
+    className: PropTypes.string, 
+    name:PropTypes.string,
+    placeholder:PropTypes.string,
+    type:PropTypes.string,
+    label:PropTypes.string,
+    prefix:PropTypes.node,
+    suffix:PropTypes.node,
+    shape: PropTypes.oneOf(["square", "round"]),
+    size: PropTypes.oneOf(["sm", "md", "xs", "lg"]),
+    variant: PropTypes.oneOf(["fill", "outline"]),
+    color: PropTypes.oneOf([
+        "black_900_02", 
+        "gray_300_01",
+        "white_A700",
+        "blue_gray_100_01",
+    ]),
+};
+
+export { Input };
