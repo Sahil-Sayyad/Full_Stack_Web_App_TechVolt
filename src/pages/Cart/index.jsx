7c808c35f06c2eb2bf9baf72cdf97060857ebbca
sahil <igsahilsayyad@gmail.com>
2024-06-19 09:24:19 +0530

    cart component done

--- src/pages/Cart/index.jsx
+++ src/pages/Cart/index.jsx
@@ -1,6 +1,9 @@
+import { Button } from "components/Button";
 import { Heading } from "components/Heading";
+import HomepageFootersection from "components/HomepageFootersection";
 import HomepageHeader from "components/HomepageHeader";
 import { Img } from "components/Img";
+import { Input } from "components/Input";
 import { Text } from "components/Text";
 
 export default function CartPage() {
@@ -9,7 +12,9 @@ export default function CartPage() {
             <div className="flex w-full flex-col gap-[39px] bg-white-A700">
                 {/*Header Section */}
                 <HomepageHeader arrowright="images/img_arrow_right_gray_700.svg" shopTwo="Shopping Cart" />
+
                 <div className="flex flex-col items-center gap-[39px]">
+
                     <Img className="container-xs h-[96px] md:h-auto md:p-5" src="images/img_stepper.svg" alt="step indicator" />
 
                     {/* Product details section */}
@@ -47,10 +52,10 @@ export default function CartPage() {
                                                 </Heading>
                                                 <div className="flex flex-wrap gap-[7px] self-start">
                                                     <Text size="xs" as="p" className="!font-medium !text-teal-400">
-                                                     Save ₹14500.00
+                                                        Save ₹14500.00
                                                     </Text>
                                                     <Text size="xs" as="p" className="!text-gray-700 line-through">
-                                                      ₹40990.00
+                                                        ₹40990.00
                                                     </Text>
 
                                                 </div>
@@ -58,13 +63,47 @@ export default function CartPage() {
                                             </div>
 
                                         </div>
-                                        <div className="h-px bg-gray-300_03"/>
+                                        <div className="h-px bg-gray-300_03" />
                                         <div className="flex flex-col items-start gap-4">
                                             <Heading size="xs" as="h3">
                                                 Price Breakdown
                                             </Heading>
+                                            <div className="flex flex-col gap-1 self-stretch">
+                                                <div className="flex flex-wrap justify-between gap-5">
+                                                    <Text size="xs" as="p" className="!font-medium !text-gray-700">
+                                                        Price (Inclusive of all taxes)
+                                                    </Text>
+                                                    <Text size="xs" as="p" className="!font-medium !text-gray-700">
+                                                        ₹40990.00
+                                                    </Text>
+
+                                                </div>
+                                                <div className="flex flex-wrap justify-between ga-5">
+                                                    <Text size="xs" as="p" className="!font-medium !text-gray-700">
+                                                        Promotional Discount(s)
+                                                    </Text>
+                                                    <Text size="xs" as="p" className="!font-medium !text-gray-700">
+                                                        -₹14500.00
+                                                    </Text>
+
+                                                </div>
+                                                <div className="flex flex-wrap justify-between ga-5">
+                                                    <Text size="xs" as="p" className="!font-medium !text-gray-700">
+                                                        Discount
+                                                    </Text>
+                                                    <Text size="xs" as="p" className="!font-medium !text-gray-700">
+                                                        -₹750.00
+                                                    </Text>
+
+                                                </div>
+
+
+                                            </div>
 
                                         </div>
+                                        <div className="h-px bg-gray-300_03" />
+
+
 
 
                                     </div>
@@ -72,12 +111,92 @@ export default function CartPage() {
                                 </div>
 
                             </div>
+                            <div className="flex w-[39%] flex-col gap-[37px] rounded-[16px] border border-solid border-black-900_02 bg-gray-100 px-[23px] pb-6 pt-[23px] md:w-full sm:p-5">
+                                <div className="flex flex-col gap-[23px]">
+                                    <div className="flex justify-between gap-5">
+                                        <div className="flex flex-col items-start gap-2">
+                                            <Heading size="2xl" as="h4">
+                                                Total
+                                            </Heading>
+                                            <Text as="p" className="!text-gray-700">
+                                                Includes GST*
+                                            </Text>
+
+                                        </div>
+                                        <div className="flex flex-col items-end gap-2">
+                                            <Heading size="2xl" as="h4">
+                                                ₹26490.00
+                                            </Heading>
+                                            <div className="flex flex-wrap gap-[7px] self-start">
+                                                <Text as="p" className="!text-gray-700 line-through">
+                                                    ₹40990.00
+                                                </Text>
+
+                                                <Text as="p" className="!font-medium !text-teal-400">
+                                                    Save ₹14500.00
+                                                </Text>
+
+                                            </div>
+
+                                        </div>
+
+                                    </div>
+                                    <div className="h-px bg-gray-400_01" />
+                                    <Input
+                                        size="lg"
+                                        name="Coupon Code Field"
+                                        placeholder={`Apply coupon code`}
+                                        prefix={<Img src="images/img_fi_11370591.svg" alt="fi_11370591" className="h-[30px] w-[30px]" />}
+                                        className="gap-2 rounded-md border border-black-900_02 font-light p-3" />
+                                    <Heading as="h6">Price Breakup</Heading>
+
+                                    <div className="flex flex-col gap-1.5 self-stretch">
+                                        <div className="flex flex-wrap justify-between gap-5">
+                                            <Text as="p" className="!text-gray-700">
+                                                Price (Inclusive of all taxes)
+                                            </Text>
+                                            <Heading as="h6">₹40990.00</Heading>
+
+                                        </div>
+                                        <div className="flex flex-wrap justify-between gap-5">
+                                            <Text as="p" className="!text-gray-700">
+                                                Discount
+                                            </Text>
+                                            <Heading as="h6" className="!text-teal-400">
+                                                -₹14500.00
+                                            </Heading>
+
+                                        </div>
+                                        <div className="flex flex-wrap items-center justify-between gap-5">
+                                            <Text as="p" className="!text-gray-700">
+                                                Shipping Charges
+                                            </Text>
+                                            <Heading as="h6">₹40.00</Heading>
+
+                                        </div>
+
+                                    </div>
+
+
+                                </div>
+
+                                <Button
+                                    color="deep_orange_A700"
+                                    size="3xl"
+                                    className="w-full !rounded-[11px] font-bold sm:px-5">
+                                    Next
+                                </Button>
+                            </div>
 
 
                         </div>
 
                     </div>
+
                 </div>
+
+               {/**Footer Section */}
+               <HomepageFootersection/>
 
             </div>
         </>
